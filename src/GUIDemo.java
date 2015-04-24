ce0c9dbd9df252df71dba6b6cc2ba7bd0c2c3392
willycode <willy741026@gamil.com>
2015-04-24 15:25:53 +0800

    Eclipse

---
+++ src/GUIDemo.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+ 
+public class GUIDemo { 
+    public static void main(String[] args) { 
+        JFrame demo = new JFrame();
+        demo.setSize(400, 300);
+        demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        JCheckBox checkbox = new JCheckBox("JCheckBox");
+        JRadioButton radiobutton = new JRadioButton("JRadiobutton");
+        JButton button = new JButton("JButton");
+        JLabel label = new JLabel("JLabel");
+        JTextArea textarea = new JTextArea("JTextArea");
+         
+        demo.getContentPane().add(BorderLayout.EAST, checkbox);
+        demo.getContentPane().add(BorderLayout.WEST, radiobutton);
+        demo.getContentPane().add(BorderLayout.SOUTH, button);
+        demo.getContentPane().add(BorderLayout.NORTH , label);
+        demo.getContentPane().add(BorderLayout.CENTER, textarea);
+         
+        demo.setVisible(true);
+    }
+}
